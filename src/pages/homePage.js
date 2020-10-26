a0015f19c3d1d40048c13c2c6186d0d4f55a74b6
Ziqi Zhou <20091593@mail.wit.ie>
2020-10-26 11:55:05 +0000

    Added routing and live data from API

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React ,{useState, useEffect }from "react";
 import Header from "../components/headerMovieList";
 import MovieList from "../components/movieList";
 import FilterControls from "../components/filterControls";
 
-const MovieListPage = (props) => {
-    const movies = props.movies;
+
+const MovieListPage = () => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
+    )
+      .then(res => res.json())
+      .then(json => {
+        console.log(json)
+        return json.results
+      })
+      .then(movies => {
+        setMovies(movies);
+      });
+  }, []);
   return (
     <div>
       <Header numMovies={movies.length} />
