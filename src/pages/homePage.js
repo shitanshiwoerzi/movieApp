354343f67a1c512d28dc27a45eded9bc89fa7e73
Ziqi Zhou <20091593@mail.wit.ie>
2020-11-04 14:15:24 +0000

    Fix buttons on movie list pages

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import PageTemplate from '../components/templateMovieListPage'
 import {MoviesContext} from '../contexts/moviesContext'
+import AddToFavoritesButton from '../components/buttons/addToFavorites'
 
   const MovieListPage = () => {
   const context = useContext(MoviesContext) 
@@ -9,7 +10,9 @@ import {MoviesContext} from '../contexts/moviesContext'
       <PageTemplate 
         title='All Movies'
         movies={context.movies}
-        buttonHandler={context.addToFavorites}
+        action={(movie) => {
+          return <AddToFavoritesButton movie={movie} /> 
+        }}
       />
   );
 };
